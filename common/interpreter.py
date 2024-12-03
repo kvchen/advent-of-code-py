4c1f9d181aab4f6787c8137b521f15fac88b62a1
Kevin Chen <keffcat@gmail.com>
2024-12-02 23:00:49 -0800

    Split out interpreter class

---
+++ common/interpreter.py
@@ -0,0 +1,76 @@
+import re
+from dataclasses import dataclass
+from typing import TextIO, Protocol, Self, Iterator
+
+
+class InstructionBase(Protocol):
+    @staticmethod
+    def pattern() -> str: ...
+
+    @classmethod
+    def from_match(cls, match: re.Match[str]) -> Self: ...
+
+
+@dataclass
+class MulInstruction(InstructionBase):
+    x: int
+    y: int
+
+    @staticmethod
+    def pattern() -> str:
+        return r"mul\((?P<mul_x>\d+),(?P<mul_y>\d+)\)"
+
+    @classmethod
+    def from_match(cls, match: re.Match[str]) -> Self:
+        x = int(match.group("mul_x"))
+        y = int(match.group("mul_y"))
+        return cls(x=x, y=y)
+
+
+@dataclass
+class DoInstruction(InstructionBase):
+    @staticmethod
+    def pattern() -> str:
+        return r"do\(\)"
+
+    @classmethod
+    def from_match(cls, match: re.Match[str]) -> Self:
+        return cls()
+
+
+@dataclass
+class DontInstruction(InstructionBase):
+    @staticmethod
+    def pattern() -> str:
+        return r"don't\(\)"
+
+    @classmethod
+    def from_match(cls, match: re.Match[str]) -> Self:
+        return cls()
+
+
+class InterpreterBase(Protocol):
+    def handle(self, instruction: InstructionBase) -> None: ...
+
+    def run(self, infile: TextIO) -> None:
+        for instruction in self._instructions(infile):
+            self.handle(instruction)
+
+
+    def _instructions(self, infile: TextIO) -> Iterator[InstructionBase]:
+        for line in infile:
+            for match in re.finditer(self._tokenizer_pattern(), line):
+                yield self._instruction_from_match(match)
+
+    def _tokenizer_pattern(self) -> str:
+        return "|".join(
+            f"(?P<{instruction_cls.__name__}>{instruction_cls.pattern()})"
+            for instruction_cls in InstructionBase.__subclasses__()
+        )
+
+    def _instruction_from_match(self, match: re.Match[str]) -> InstructionBase:
+        for instruction_cls in InstructionBase.__subclasses__():
+            if match.group(instruction_cls.__name__):
+                return instruction_cls.from_match(match)
+
+        raise Exception(f"Unknown instruction: {match}")
--- solutions_2024/day_03.py
+++ solutions_2024/day_03.py
@@ -1,115 +1,64 @@
-import re
-from dataclasses import dataclass
-from typing import TextIO, Protocol, Self
+from typing import TextIO
 
+from common.interpreter import (
+    InterpreterBase,
+    DoInstruction,
+    DontInstruction,
+    MulInstruction,
+    InstructionBase
+)
 from common.solution import SolutionBase
 
 
-class InstructionBase(Protocol):
-    @staticmethod
-    def pattern() -> str: ...
-
-    @classmethod
-    def from_match(cls, match: re.Match[str]) -> Self: ...
+TParsed = TextIO
 
 
-@dataclass
-class MulInstruction(InstructionBase):
-    x: int
-    y: int
-
-    @staticmethod
-    def pattern() -> str:
-        return r"mul\((?P<mul_x>\d+),(?P<mul_y>\d+)\)"
-
-    @classmethod
-    def from_match(cls, match: re.Match[str]) -> Self:
-        x = int(match.group("mul_x"))
-        y = int(match.group("mul_y"))
-        return cls(x=x, y=y)
+class Part01Interpreter(InterpreterBase):
+    def __init__(self):
+        self.total = 0
+    
+    def handle(self, instruction: InstructionBase):
+        match instruction:
+            case MulInstruction(x, y):
+                self.total += x * y
+            case _:
+                pass
 
 
-@dataclass
-class DoInstruction(InstructionBase):
-    @staticmethod
-    def pattern() -> str:
-        return r"do\(\)"
+class Part02Interpreter(InterpreterBase):
+    def __init__(self):
+        self.total = 0
+        self.is_mul_enabled = True
+    
+    def handle(self, instruction: InstructionBase):
+        match instruction:
+            case MulInstruction(x, y):
+                if self.is_mul_enabled:
+                    self.total += x * y
+            case DoInstruction():
+                self.is_mul_enabled = True
+            case DontInstruction():
+                self.is_mul_enabled = False
+            case _:
+                pass
 
-    @classmethod
-    def from_match(cls, match: re.Match[str]) -> Self:
-        return cls()
-
-
-@dataclass
-class DontInstruction(InstructionBase):
-    @staticmethod
-    def pattern() -> str:
-        return r"don't\(\)"
-
-    @classmethod
-    def from_match(cls, match: re.Match[str]) -> Self:
-        return cls()
-
-
-Instruction = MulInstruction | DoInstruction | DontInstruction
-InstructionTypes: list[type[Instruction]] = [
-    MulInstruction,
-    DoInstruction,
-    DontInstruction,
-]
-
-
-TParsed = list[Instruction]
 
 
 class Solution(SolutionBase[TParsed], year=2024, day=3):
     @classmethod
     def parse_input(cls, infile: TextIO) -> TParsed:
-        tokenizer_regex = "|".join(
-            f"(?P<{instruction_cls.__name__}>{instruction_cls.pattern()})"
-            for instruction_cls in InstructionTypes
-        )
-
-        parsed: TParsed = []
-        for line in infile:
-            for match in re.finditer(tokenizer_regex, line):
-                parsed.append(cls.get_instruction(match))
-
-        return parsed
-
-    @classmethod
-    def get_instruction(cls, match: re.Match[str]) -> Instruction:
-        for instruction_cls in InstructionTypes:
-            if match.group(instruction_cls.__name__):
-                return instruction_cls.from_match(match)
-
-        raise Exception(f"Unknown instruction: {match}")
+        return infile
 
     @classmethod
     def part_01(cls, parsed_input: TParsed) -> int:
-        total = 0
-        for instruction in parsed_input:
-            match instruction:
-                case MulInstruction(x, y):
-                    total += x * y
-                case _:
-                    pass
+        interpreter = Part01Interpreter()
+        interpreter.run(parsed_input)
 
-        return total
+        return interpreter.total
 
     @classmethod
     def part_02(cls, parsed_input: TParsed) -> int:
-        total = 0
-        is_mul_enabled = True
+        interpreter = Part02Interpreter()
+        interpreter.run(parsed_input)
 
-        for instruction in parsed_input:
-            match instruction:
-                case MulInstruction(x, y):
-                    if is_mul_enabled:
-                        total += x * y
-                case DoInstruction():
-                    is_mul_enabled = True
-                case DontInstruction():
-                    is_mul_enabled = False
-
-        return total
+        return interpreter.total
