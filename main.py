b2fb6c23e028fba2773d894277fb6bdaed74fc5f
Kevin Chen <keffcat@gmail.com>
2024-12-07 21:55:55 -0800

    Add day 8 solution:

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ from typing import TextIO
 @click.option("--year", type=int, default=2024)
 @click.option("--day", type=int, required=True)
 @click.option("--part", type=click.Choice(list(SolutionBase.Part)), required=True)
-@click.option("--input", type=click.File("r"), required=True)
-def main(year: int, day: int, part: SolutionBase.Part, input: TextIO):
+@click.option("--input", type=click.File("r"))
+def main(year: int, day: int, part: SolutionBase.Part, input: TextIO | None):
     base_path = Path(f"solutions_{year}")
     import_solutions(base_path)
+
+    if input is None:
+        input = open(base_path / "inputs" / f"{day:02}", "r")
 
     solution = SolutionBase.get_solution(year, day)
     answer = solution.solve(input, part)
---
+++ solutions_2024/day_08.py
@@ -0,0 +1,81 @@
+from collections import defaultdict
+from dataclasses import dataclass, field
+from itertools import combinations
+from typing import TextIO
+
+from common.solution import SolutionBase
+
+
+@dataclass(frozen=True)
+class Position:
+    row: int
+    col: int
+
+    def offset(self, other: "Position") -> tuple[int, int]:
+        return self.row - other.row, self.col - other.col
+
+    def with_offset(self, offset: tuple[int, int]) -> "Position":
+        return Position(self.row + offset[0], self.col + offset[1])
+
+
+@dataclass
+class Map:
+    width: int
+    height: int
+    antennas: dict[str, set[Position]] = field(default_factory=lambda: defaultdict(set))
+
+    def in_bounds(self, position: Position) -> bool:
+        return 0 <= position.row < self.height and 0 <= position.col < self.width
+
+
+TParsed = Map
+
+
+class Solution(SolutionBase[TParsed], year=2024, day=8):
+    @classmethod
+    def parse_input(cls, infile: TextIO) -> TParsed:
+        antennas: dict[str, set[Position]] = defaultdict(set)
+        for row, line in enumerate(infile):
+            for col, char in enumerate(line.strip()):
+                if char not in {'.', '#'}:
+                    position = Position(row, col)
+                    antennas[char].add(position)
+
+        map = Map(width=col+1, height=row+1, antennas=antennas)  # type: ignore
+        return map
+
+    @classmethod
+    def part_01(cls, parsed_input: TParsed) -> int:
+        locations: set[Position] = set()
+        for positions in parsed_input.antennas.values():
+            for p1, p2 in combinations(positions, 2):
+                offset = p1.offset(p2)
+                an1 = p1.with_offset(offset)
+                an2 = p2.with_offset((-offset[0], -offset[1]))
+
+                if parsed_input.in_bounds(an1):
+                    locations.add(an1)
+                if parsed_input.in_bounds(an2):
+                    locations.add(an2)
+
+        return len(locations)
+
+    @classmethod
+    def part_02(cls, parsed_input: TParsed) -> int:
+        locations: set[Position] = set()
+        for positions in parsed_input.antennas.values():
+            for p1, p2 in combinations(positions, 2):
+                offset = p1.offset(p2)
+
+                an1 = p1
+                while parsed_input.in_bounds(an1):
+                    locations.add(an1)
+                    an1 = an1.with_offset(offset)
+
+                neg_offset = (-offset[0], -offset[1])
+                an2 = p2
+                while parsed_input.in_bounds(an2):
+                    locations.add(an2)
+                    an2 = an2.with_offset(neg_offset)
+
+        return len(locations)
